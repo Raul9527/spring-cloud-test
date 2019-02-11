780798a83d0e5d841e6bd8189455d7d178cb7f32
raul <673237461@qq.com>
2019-02-11 19:55:51 +0800

    test

--- zuul/src/main/java/com/lrg/Test.java
+++ zuul/src/main/java/com/lrg/Test.java
@@ -3,7 +3,7 @@ package com.lrg;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+//import java.util.Optional;
 
 public class Test<T> {
     public static void main(String[] args){
@@ -11,8 +11,8 @@ public class Test<T> {
     }
     public List<? extends T> main(T... a){
         Map map = new HashMap();
-        Optional<String> optional = Optional.of("zhangsan");
-        System.out.println(optional.orElseGet(()->"ÕÅÈý"));
+        /*Optional<String> optional = Optional.of("zhangsan");
+        System.out.println(optional.orElseGet(()->"å‘µå‘µ"));*/
         return null;
     }
 }
